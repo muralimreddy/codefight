125d2786d1b57ab7f5b25e2455145471fcf6b1c0
Murali Reddy <muralidharan.manohar.reddy@macys.com>
2020-04-13 00:00:26 -0400

    Apr 2020 code challanges

--- src/main/java/com/codefight/challanges/InsertValueIntoSortedLinkedList.java
+++ src/main/java/com/codefight/challanges/InsertValueIntoSortedLinkedList.java
@@ -1,5 +1,27 @@
 package com.codefight.challanges;
-
+/**
+ * Note: Your solution should have O(n) time complexity, where n is the number of elements in l, since this is what you will be
+ * asked to accomplish in an interview.
+ *
+ * You have a singly linked list l, which is sorted in strictly increasing order, and an integer value. Add value to the list l,
+ * preserving its original sorting.
+ *
+ * Note: in examples below and tests preview linked lists are presented as arrays just for simplicity of visualization: in
+ * real data you will be given a head node l of the linked list
+ *
+ * Example
+ *
+ * For l = [1, 3, 4, 6] and value = 5, the output should be
+ * insertValueIntoSortedLinkedList(l, value) = [1, 3, 4, 5, 6];
+ * For l = [1, 3, 4, 6] and value = 10, the output should be
+ * insertValueIntoSortedLinkedList(l, value) = [1, 3, 4, 6, 10];
+ * For l = [1, 3, 4, 6] and value = 0, the output should be
+ * insertValueIntoSortedLinkedList(l, value) = [0, 1, 3, 4, 6].
+ * For l=[] and value 239, the output should be
+ * insertValueIntoSortedLinkedList(l, value) = [239].
+ * For l=[1,3,4] and value -100, the output should be
+ * insertValueIntoSortedLinkedList(l, value) = [-100, 1,3,4].
+ */
 public class InsertValueIntoSortedLinkedList {
     public static void main(String[] args){
         InsertValueIntoSortedLinkedList ins = new InsertValueIntoSortedLinkedList();
